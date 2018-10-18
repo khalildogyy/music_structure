5febbddf1650621275f3f3da2232a49724ad3e25
khalildogyy <499432435@qq.com>
2018-10-18 14:10:05 +0800

    Update accent.py

--- accent.py
+++ accent.py
@@ -10,3 +10,11 @@ y, sr = librosa.load(filename,sr=None)
 o_envs = librosa.onset.onset_strength(y, sr=sr)
 times = librosa.frames_to_time(np.arange(len(o_envs)), sr=sr)
 
+xn
+mu = 0.1
+yn = np.log(1 + mu * xn) / np.log(1 + mu)
+
+#插值
+zn = [0] * (2*len(yn) - 1)  
+for i in range(len(yn)):
+	zn[2*i] = yn[i]
